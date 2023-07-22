59cd81aab58e8eca3b99347956582f514a90a63f
Manteiga <rafael.manteiga@hotmail.com>
2023-07-22 02:31:19 -0300

    Add files via upload

--- taylor.py
+++ taylor.py
@@ -3,30 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def taylor(f, a, n):
-    x = sp.Symbol('x')
+x = sp.Symbol('x')
+entrada = input("\nDigite a função: ")
+f = sp.sympify(entrada)
+a = int(input("Digite o ponto: "))
+n = int(input("Digite até qual quantidade de termos você deseja no gráfico: "))
+
+for n in range(2,n+1):
     serie = sp.series(f, x, a, n, "+").removeO()
-    func = sp.lambdify(x, f, modules=['numpy'])
     func_serie_taylor = sp.lambdify(x, serie, modules=['numpy'])
-
     valores_x = np.linspace(-10, 10, 1000)
-    valores_y = func(valores_x)
     valores_y_taylor = func_serie_taylor(valores_x)
+    sns.lineplot(x=valores_x, y=valores_y_taylor, label="Série de Taylor de grau "+str(n-1)+" de f(x)")
 
-    sns.lineplot(x=valores_x, y=valores_y_taylor, label="Série de Taylor de f(x)")
-    sns.lineplot(x=valores_x, y=valores_y, label="f(x)")
-    plt.ylim(-10,10)
-    plt.show()
+func = sp.lambdify(x, f, modules=['numpy'])
+valores_x = np.linspace(-10, 10, 1000)
+valores_y = func(valores_x)
+sns.lineplot(x=valores_x, y=valores_y, label="f(x)")
 
-    return serie
-
-def aproximacao_taylor():
-    entrada = input("\nDigite a função: ")
-    f = sp.sympify(entrada)
-    a = int(input("Digite o ponto: "))
-    n = int(input("Digite a quantidade de termos que deseja na série: "))
-    print("\nA função aproximada é:", taylor(f, a, n))
-    print("")
-
-if __name__ == "__main__":
-    aproximacao_taylor()
+print("")
+plt.ylim(-10,10)
+plt.show()

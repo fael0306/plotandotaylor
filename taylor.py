72f7372e8514f4a2cb70f4336d70f3aaff3b20aa
Manteiga <rafael.manteiga@hotmail.com>
2023-07-22 02:40:26 -0300

    Add files via upload

--- taylor.py
+++ taylor.py
@@ -9,12 +9,12 @@ f = sp.sympify(entrada)
 a = int(input("Digite o ponto: "))
 n = int(input("Digite até qual quantidade de termos você deseja no gráfico: "))
 
-for n in range(2,n+1):
-    serie = sp.series(f, x, a, n, "+").removeO()
+for k in range(2,n+1):
+    serie = sp.series(f, x, a, k, "+").removeO()
     func_serie_taylor = sp.lambdify(x, serie, modules=['numpy'])
     valores_x = np.linspace(-10, 10, 1000)
     valores_y_taylor = func_serie_taylor(valores_x)
-    sns.lineplot(x=valores_x, y=valores_y_taylor, label="Série de Taylor de grau "+str(n-1)+" de f(x)")
+    sns.lineplot(x=valores_x, y=valores_y_taylor, label="Série de Taylor de grau "+str(k-1)+" de f(x)")
 
 func = sp.lambdify(x, f, modules=['numpy'])
 valores_x = np.linspace(-10, 10, 1000)
@@ -22,5 +22,5 @@ valores_y = func(valores_x)
 sns.lineplot(x=valores_x, y=valores_y, label="f(x)")
 
 print("")
-plt.ylim(-10,10)
+plt.ylim(-100,100)
 plt.show()
